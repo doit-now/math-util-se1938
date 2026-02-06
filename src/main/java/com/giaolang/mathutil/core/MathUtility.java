16dfc560c90c67f14632a8480b9738bf86c0a11a
doit-now <hoang.nguyenthe@gmail.com>
2026-02-06 14:03:14 +0700

    1st upload

---
+++ src/main/java/com/giaolang/mathutil/core/MathUtility.java
@@ -0,0 +1,27 @@
+package com.giaolang.mathutil.core;
+
+//Đây là thư viện toán học, mô phỏng giống thư viện chuẩn Math. của JDK
+//Các thư viện mang tính chất tiện ích, đồ dùng xài chung cho các nơi, cung cấp các tool nho nhỏ, thì ta sẽ thiết kế các hàm là static!!!!!!!!!!!!!!!!!!!
+public class MathUtility {
+
+    //hàm tính n! = 1.2.3...n
+    //ko có giai thừa số âm
+    //20! vừa đủ kiểu long - 18 con số 0, 21! kiểu long chứa ko nổi
+    //0! = 1 quy ước
+    public static long getFactorial(int ngoctrinh) {
+
+        long result;
+
+        if (ngoctrinh < 0 || ngoctrinh > 20) {
+            throw new IllegalArgumentException("n must be between 0 and 20");
+        }
+
+        if (ngoctrinh == 0)
+            return 1;
+
+        result = 10;
+        for (int i = 1; i <= ngoctrinh; i++)
+            result = result * i;
+        return result;
+    }
+}
